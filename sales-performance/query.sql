5224b3b7ed6c93f18b0fcb5e3fbec3fbaf905fd3
Egira Adhani Khairunnisa <72202488+egiraak@users.noreply.github.com>
2025-10-02 22:46:54 +0700

    Update query.sql

--- sales-performance/query.sql
+++ sales-performance/query.sql
@@ -15,3 +15,13 @@ FROM ranked
 WHERE rn <= 3
 ORDER BY "Region", total_revenue DESC;
 
+
+SELECT
+  "Product_Category",
+  SUM("Revenue") AS revenue,
+  SUM("Profit") AS profit,
+  ROUND( (SUM("Profit") * 1.0) / NULLIF(SUM("Revenue"),0), 4)*100 AS profit_margin,
+  ROUND(AVG("Discount_Percentage"),2) AS avg_discount
+FROM d2
+GROUP BY "Product_Category"
+ORDER BY profit_margin DESC;
